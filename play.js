2ce49c3008d71f1cb10a23332ca51d8f81d666b4
JosueEtta <atabejosue@gmail.com>
2025-07-25 22:46:50 +0000

    icons aded with style and fontawesome installed

--- play.js
+++ play.js
@@ -3,17 +3,19 @@ const playPauseButton = document.getElementById("playPauseButton");
 const playBtn = document.getElementById("playBtn");
 const pauseBtn = document.getElementById("pauseBtn");
 const audioImg = document.getElementById("audio-img");
+const options = document.querySelectorAll(".options img");
+
 function playOrStopMusic() {
   let playState = music.paused ? "play" : "pause";
   if (playState === "play") {
-    pauseBtn.style.display = "none";
+    pauseBtn.style.display = "flex";
     audioImg.classList.add("rotate");
-    playBtn.style.display = "flex";
+    playBtn.style.display = "none";
     music.play();
   } else {
-    pauseBtn.style.display = "flex";
+    pauseBtn.style.display = "none";
     audioImg.classList.remove("rotate");
-    playBtn.style.display = "none";
+    playBtn.style.display = "flex";
     music.pause();
   }
 }
